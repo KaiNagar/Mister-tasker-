b38e949b96fa2c545ce6e80bb837ec4fe2d638a3
BarBronshtein <barbronsthein@gmail.com>
2022-08-04 18:44:23 +0300

    added save task in task-edit

--- src/views/task-edit.vue
+++ src/views/task-edit.vue
@@ -1,20 +1,23 @@
 <template>
-    <form></form>
-    id:{{ taskToEdit._id }}
-    <input type="checkbox" @change="changeTaskStatus">Status: {{ taskToEdit.status }}
-    Description:<input type="text" v-model="taskToEdit.description">
-    Title:<input type="text" v-model="taskToEdit.title">
-    Importance:<input min="1" max="3" type="number" v-model="taskToEdit.importance">
-    Done at:{{ taskToEdit.doneAt ? new Date(taskToEdit.doneAt).toLocaleTimeString('en-US') : 'Incomplete' }}
+    <form @submit="saveTask">
+        id:{{ taskToEdit._id }}
+        <input type="checkbox" @change="changeTaskStatus">Status: {{ taskToEdit.status }}
+        Description:<input type="text" v-model="taskToEdit.description">
+        Title:<input type="text" v-model="taskToEdit.title">
+        Importance:<input min="1" max="3" type="number" v-model="taskToEdit.importance">
+        Done at:{{ taskToEdit.doneAt ? new Date(taskToEdit.doneAt).toLocaleTimeString('en-US') : 'Incomplete' }}
+        <button class="btn-save">Save</button>
+    </form>
 </template>
 
 <script>
 import { firebaseService } from '../services/firebase.service'
+import { taskService } from '../services/task.service'
 export default {
     async created() {
         const { _id } = this.$route.params
         this.taskToEdit = _id ?
-            await firebaseService.getById(_id) : this.getEmptyTask
+            await firebaseService.getById(_id) : taskService.getEmptyTask()
     },
     data() {
         return { taskToEdit: null, }
@@ -28,20 +31,13 @@ export default {
                 this.taskToEdit.status = 'done'
                 this.taskToEdit.doneAt = Date.now()
             }
+        },
+        saveTask() {
+            // New task
+            !this.taskToEdit._id && firebaseService.addTask(this.taskToEdit)
+            // Already exists updating task
+            this.taskToEdit._id && firebaseService.updateTask(this.taskToEdit)
         }
     },
-    computed: {
-        getEmptyTask() {
-            return {
-                title: '',
-                description: '',
-                importance: 1,
-                status: 'in-progress',
-                createdAt: Date.now(),
-                doneAt: null,
-            }
-        }
-
-    }
 }
 </script>
